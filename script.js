1f0f73062c755a1aaa71a4cf3743197a5faba5d3
konnenhannaford <konnen23@gmail.com>
2021-09-13 21:42:00 +1000

    made several changes to layout of html and css.  Started addign to Java

--- script.js
+++ script.js
@@ -1,25 +1,74 @@
-// this is nicks code to get clock to work
-var timeEl = document.querySelector(".timer-count");
-var mainEl = document.querySelector("main")
+var informationButton = document.querySelector("#information-button");
+var testInfo = document.querySelector(".test-info");
+var begin = document.querySelector("#begin");
+var testQuestions = document.querySelector(".test-questions");
 
-var secondsLeft = 10;
 
-function setTime () {
 
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft;
 
-        if (secondsLeft === 0) {
-        clearInterval(timerInterval);
+var questions = document.querySelector(".questions");
+var questionNumber = document.querySelector(".question-number");
+var answerOptions = document.querySelector(".answer-options");
+var clockMessage = document.querySelector(".clock-message");
+var countdown = document.querySelector(".countdown");
+var clock = document.querySelector(".clock");
+var submitQuestion = document.querySelector(".submit-question");
+var results = document.querySelector(".results");
+var resultsMessage= document.querySelector(".results-message");
+var score = document.querySelector(".score");
+var endOptions = document.querySelector(".end-options");
+var tryAgain = document.querySelector(".try-again");
+var giveUp = document.querySelector(".give-up");
+var submitQuestion = document.querySelector(".Begin");
 
-        sendMessage();
-        }
-    }, 1000);
+document.querySelector("#information-button").addEventListener("click", displaytextinfo)
+
+function displaytextinfo () {
+    button
 }
 
-function sendMessage() {
-    timeEl.textConent = " LOSER!!! "
-}
+function startQuiz() {
+    currentQuestion = questions[currentQuestionIndex];
+    rules.setAttribute("class", "hidden");
+    quiz.removeAttribute("class")
 
-setTime();
+var questions = [
+        {
+
+        question: "What comes after 3?",
+        correctanswer: 4,
+        answer1: "two", 
+        answer2: "three", 
+        answer3: "four", 
+        answer4: "five",
+      },
+        {
+
+        question: "What comes after 3?",
+        correctanswer: 4,
+        answer1: "two", 
+        answer2: "three", 
+        answer3: "four", 
+        answer4: "five",
+      },
+        {
+        
+        question: "What comes after 3?",
+        correctanswer: 4,
+        answer1: "two", 
+        answer2: "three", 
+        answer3: "four", 
+        answer4: "five",
+      },
+        {
+
+        question: "What comes after 3?",
+        correctanswer: 4,
+        answer1: "two", 
+        answer2: "three", 
+        answer3: "four", 
+        answer4: "five",
+      },
+    
+    ];
+}
