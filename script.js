21a4d70b397601556a56e7c376dd755e31364ddd
konnenhannaford <konnen23@gmail.com>
2021-09-13 22:06:59 +1000

    udpates to java

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
+// i get this
 var informationButton = document.querySelector("#information-button");
 var testInfo = document.querySelector(".test-info");
 var begin = document.querySelector("#begin");
 var testQuestions = document.querySelector(".test-questions");
 
-
-
-
+// not sure on this
 var questions = document.querySelector(".questions");
-var questionNumber = document.querySelector(".question-number");
 var answerOptions = document.querySelector(".answer-options");
-var clockMessage = document.querySelector(".clock-message");
-var countdown = document.querySelector(".countdown");
-var clock = document.querySelector(".clock");
-var submitQuestion = document.querySelector(".submit-question");
-var results = document.querySelector(".results");
-var resultsMessage= document.querySelector(".results-message");
-var score = document.querySelector(".score");
-var endOptions = document.querySelector(".end-options");
-var tryAgain = document.querySelector(".try-again");
-var giveUp = document.querySelector(".give-up");
-var submitQuestion = document.querySelector(".Begin");
+var submitQuestion = document.querySelector("#submit-question");
+
+// need to review
+// var clockMessage = document.querySelector(".clock-message");
+// var countdown = document.querySelector(".countdown");
+// var clock = document.querySelector(".clock");
+// var submitQuestion = document.querySelector(".submit-question");
+// var results = document.querySelector(".results");
+// var resultsMessage= document.querySelector(".results-message");
+// var score = document.querySelector(".score");
+// var endOptions = document.querySelector(".end-options");
+// var tryAgain = document.querySelector(".try-again");
+// var giveUp = document.querySelector(".give-up");
+// var submitQuestion = document.querySelector(".Begin");
 
 document.querySelector("#information-button").addEventListener("click", displaytextinfo)
 
-function displaytextinfo () {
+function displaytextinfo () { document.querySelector()
     button
 }
 
-function startQuiz() {
-    currentQuestion = questions[currentQuestionIndex];
-    rules.setAttribute("class", "hidden");
-    quiz.removeAttribute("class")
+function displaytextinfo () {
+    var element = document.getElementById("information-button");
+    element.classList.toggle("test-info");
+
 
 var questions = [
         {
