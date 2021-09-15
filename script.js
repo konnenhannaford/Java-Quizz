7fc77e8c2c30f92ecd3894f642619b67a88df120
konnenhannaford <konnen23@gmail.com>
2021-09-15 17:59:19 +1000

    trying to make the next button function work

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-// i get this
-var informationButton = document.querySelector("#information-button");
+// these will make buttons work
+var information = document.querySelector("#information");
 var testInfo = document.querySelector(".test-info");
 var begin = document.querySelector("#begin");
 var testQuestions = document.querySelector(".test-questions");
+var submit= document.querySelector("#submit");
+var results = document.querySelector(".results");
+// these 2 will loop back to the start of the quiz or to the homepage 
+var restart = document.querySelector("#restart");
+var titlePage = document.querySelector("title-page");
+var container = document.querySelector("container");
 
 // not sure on this
 var questions = document.querySelector(".questions");
-var answerOptions = document.querySelector(".answer-options");
+var answerOption1 = document.querySelector(".answer-option1");
+var answerOption2 = document.querySelector(".answer-option2");
+var answerOption3 = document.querySelector(".answer-option3");
+var answerOption4 = document.querySelector(".answer-option4");
 var submitQuestion = document.querySelector("#submit-question");
 
 // need to review
@@ -14,23 +23,59 @@ var submitQuestion = document.querySelector("#submit-question");
 // var countdown = document.querySelector(".countdown");
 // var clock = document.querySelector(".clock");
 // var results = document.querySelector(".results");
-// var resultsMessage= document.querySelector(".results-message");
-// var score = document.querySelector(".score");
-// var tryAgain = document.querySelector(".try-again");
-// var giveUp = document.querySelector(".give-up");
-/
-element.getElementById("myBtn").addEventListener("click", displaytestinfo);
+// var testResult= document.querySelector(".test-result");
+// var username = document.querySelector(".username");
+// var scoreboard = document.querySelector(".scoreboard");
 
-function displaytestinfo () {
-    var element = document.getElementById("#information-button");
-        if (element.style.display === "none") {
-            element.style.display = "block";
-        } else {
-            element.style.display = "none";
+
+// if startQuiz button clicked
+information.onclick = ()=>{
+    titlePage.classList.replace("title-page")}
+
+// if startQuiz button clicked
+    document.getElementById("information").addEventListener("click", displaytestinfo);
+
+    function displaytestinfo () {
+        var displayinformation = document.getElementById("information");
+            if (displayinformation.style.display === "none") {
+                displayinformation.style.display = "block";
+            } else {
+                displayinformation.style.display = "none";
+            }
         }
-      
-    
 
+    document.getElementById("begin").addEventListener("click", displaytestQuestions);
+
+    function displaytestQuestions () {
+        var displayquestions = document.getElementById("begin");
+            if (displayquestions.style.display === "none") {
+                displayquestions.style.display = "block";
+            } else {
+                displayquestions.style.display = "none";
+            }
+    }    
+
+    document.getElementById("submit").addEventListener("click", displayresults);
+
+    function displayresults () {
+        var displayscores = document.getElementById("submit");
+            if (displayscores.style.display === "none") {
+                displayscores.style.display = "block";
+            } else {
+                displayscores.style.display = "none";
+            }
+
+    document.getElementById("restart").addEventListener("click", displaytestinfo);
+
+    function displaytestinfo () {
+        displayinformation = document.getElementById("restart");
+            if (displayinformation.style.display === "none") {
+                displayinformation.style.display = "block";
+            } else {
+                displayinformation.style.display = "none";
+            }
+        }
+    }
 
 // var questions = [
 //         {
@@ -71,4 +116,4 @@ function displaytestinfo () {
 //       },
     
 //     ];
-// }
+// .
